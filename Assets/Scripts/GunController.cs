b7c290d91ea7682c6ed1d0363e516df9873249a9
Magda <mag.lakomiec@gmail.com>
2017-12-04 23:08:30 +0100

    Added controller script

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+//    private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    public bool triggerButtonDown = false;
+    public bool triggerButtonUp = false;
+    public bool triggerButtonPressed = false;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized");
+            return;
+        }
+
+        if (controller.GetPressDown(triggerButton))
+        {
+            Debug.Log("Naciśnięto trigger button");
+        }
+        if (controller.GetPressUp(triggerButton))
+        {
+            Debug.Log("Puszczono trigger button");
+        }
+       
+    }
+}
